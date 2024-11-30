2dcfa39d94795103f7975d52248620c7b01c147e
Thanh Nguyen <thanh.nguyen@aeris.net>
2024-11-30 20:20:12 +0700

    rebuild project

--- src/services/mutations.ts
+++
@@ -1,5 +0,0 @@
-export const MUTATION_KEY = {
-  ADD_NOTE: "ADD_NOTE",
-  UPDATE_NOTE: "UPDATE_NOTE",
-  DELETE_NOTE: "DELETE_NOTE",
-};
--- src/services/queries.ts
+++
@@ -1,4 +0,0 @@
-export const QUERY_KEY = {
-  GET_NOTES: "GET_NOTES",
-  GET_NOTE_DETAILS: "GET_NOTE_DETAILS",
-};
